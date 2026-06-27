4d8a8a23907dc5351c49c12b5b4b9b5151095e42
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-06-27 20:36:33 +0530

    Add files via upload

---
+++ floyads_trig.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter a rows : ";
+    cin>>m;
+    int a = 1;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=i; j++){
+            cout<<a<<" ";
+            a++;
+        }
+        cout<<endl;
+    }
+}
